a7246c259b29c4eb759e0dbdd1b2c96dc61203b5
akashsharma1706 <131576871+akashsharma1706@users.noreply.github.com>
2024-01-21 19:53:49 +0530

    Add files via upload
    Added eventHandler on a button

---
+++ ExpenseItem.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
+
+  const [amount, setAmount] = useState(props.amount);
+
+
+  const clickHandler = () => {
+    setAmount('100');
+    console.log(amount);
+  };
+
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${amount}</div>
+        <button onClick={clickHandler}>Update price</button>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
