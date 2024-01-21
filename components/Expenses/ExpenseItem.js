87fd794e3b908ed494982c1f8acd9ef09f3f9ae8
akashsharma1706 <131576871+akashsharma1706@users.noreply.github.com>
2024-01-21 15:12:57 +0530

    Add files via upload
    Added eventHandler on a button

--- components/Expenses/ExpenseItem.js
+++ components/Expenses/ExpenseItem.js
@@ -1,21 +1,23 @@
 import React from 'react';
+
 import ExpenseDate from './ExpenseDate';
-import ExpenseDetails from './ExpenseDetails';
-import Card from '../UI/Card'; 
+import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-    return(
-      <Card className="expense-item">
-        <ExpenseDate date={props.date}/>
-        <ExpenseDetails amount={props.amount} location={props.location} title={props.title} />
-         {/* <div className="expense-item__description">
-          <h2>{props.title}</h2>  
-          <div className="expense__location">{props.location}</div>
-           <div className="expense-item__price">Rs.{props.amount}</div>
-        </div> */}
-      </Card>
-        );
+  const clickHandler = () => {
+    console.log('Deleted expense');
+  }
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+        <button onClick={clickHandler}>Delete Expense</button>
+      </div>
+    </Card>
+  );
 }
 
 export default ExpenseItem;
