09b5f9d5cc8ac89aac3ad1df1eaf942d1865893e
akashsharma1706 <131576871+akashsharma1706@users.noreply.github.com>
2024-01-22 15:33:17 +0530

    Add files via upload

--- components/Expenses/ExpenseItem.js
+++ components/Expenses/ExpenseItem.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+
+  const [amount, setAmount] = useState(props.amount);
+
+
   const clickHandler = () => {
-    console.log('Deleted expense');
-  }
+    setAmount('100');
+    console.log(amount);
+  };
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-        <button onClick={clickHandler}>Delete Expense</button>
+        <div className='expense-item__price'>${amount}</div>
+        <button onClick={clickHandler}>Update price</button>
       </div>
     </Card>
   );
